8f468d8de7589e560794e6dd3e283a79fef6a3e5
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2022-10-06 17:19:33 +0200

    Add more tests to fix other cases
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- spec/puppet-lint/plugins/manifest_whitespace_opening_brace_spec.rb
+++ spec/puppet-lint/plugins/manifest_whitespace_opening_brace_spec.rb
@@ -31,6 +31,32 @@ describe 'manifest_whitespace_opening_brace_before' do
     end
   end
 
+  context 'inline with a function before' do
+    let(:code) do
+      <<~EOF
+        Hash( { $key => $return_value })
+      EOF
+    end
+
+    it 'should detect 1 problem' do
+      skip 'implement this test'
+      expect(problems).to have(1).problem
+    end
+  end
+
+  context 'inline with a function after' do
+    let(:code) do
+      <<~EOF
+        Hash({ $key => $return_value } )
+      EOF
+    end
+
+    it 'should detect 1 problem' do
+      skip 'implement this test'
+      expect(problems).to have(1).problem
+    end
+  end
+
   context 'inline with a function' do
     let(:code) do
       <<~EOF
